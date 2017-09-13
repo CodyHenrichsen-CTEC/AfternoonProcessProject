145375ff9df2fae5bfb3faabf1a812d75a805292
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2017-09-13 13:43:37 -0600

    Added a setter for the size data member.

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -1,5 +1,6 @@
 package process.controller;
 
+import java.util.Scanner;
 import process.model.PlayDohCircle;
 
 public class ProcessController
@@ -21,5 +22,15 @@ public class ProcessController
 		System.out.println("Look I made a circle");
 		System.out.println(firstCircle);
 		System.out.println(secondCircle);
+		
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("How big of a circle do you want??");
+		int circleSize = myScanner.nextInt();
+		
+		
+		PlayDohCircle thirdCircle = new PlayDohCircle(circleSize);
+		System.out.println(thirdCircle);
 	}
+	
+	
 }
--- src/process/model/PlayDohCircle.java
+++ src/process/model/PlayDohCircle.java
@@ -5,12 +5,23 @@ public class PlayDohCircle
 	//Declaration Section
 	//Data member section
 	private int size;
-
+	
 	public PlayDohCircle()
 	{
 		this.size = 5;
 		
 	}
+	
+	public PlayDohCircle(int big)
+	{
+		this.size = big;
+	}
+	
+	public void setSize(int newSize)
+	{
+		this.size = newSize;
+	}
+	
 	
 	public String toString()
 	{
